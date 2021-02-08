9d08abc72e937463b6086f07c388db4c695da08a
Steven Gregory <sgregory@gmail.com>
2021-02-08 16:11:51 -0600

    refactor: remove unneeded ts path

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -12,7 +12,6 @@ transpile() {
   build_dir="dist"
   interface_dir="Interfaces"
   src_path="$(get_project_name system/sn-workspace.json ACTIVE_APPLICATION)/src"
-  ts_path="$(get_project_name system/sn-workspace.json ACTIVE_APPLICATION)/ts"
   tsc
   prettier --write "$build_dir/**/*.js"
   rsync -av --progress -a --exclude="$interface_dir" "$build_dir/" "$src_path"
