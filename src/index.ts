12e41f6dec654268424165c79f428f243ec47894
Steven Gregory <sgregory@gmail.com>
2021-02-09 21:58:40 -0600

    refactor: convert getOption to arrow fn

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,25 @@ const path = require('path');
 const { description, version } = require('./../package.json');
 const { program } = require('commander');
 
+const getOption = (opts) => {
+  const option = Object.keys(opts).toString();
+  const options = {
+    build: () => {
+      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/build.sh`);
+    },
+    compile: () => {
+      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/compile.sh`);
+    },
+    sync: () => {
+      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/sync.sh`);
+    },
+    default: () => {
+      program.help();
+    }
+  };
+  return (options[option] || options['default'])();
+};
+
 (() => {
   program.description(description);
   program.version(version);
@@ -23,22 +42,3 @@ const { program } = require('commander');
   program.parse(process.argv).opts();
   getOption(program.opts());
 })();
-
-function getOption(opts) {
-  const option = Object.keys(opts).toString();
-  const options = {
-    build: () => {
-      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/build.sh`);
-    },
-    compile: () => {
-      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/compile.sh`);
-    },
-    sync: () => {
-      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/sync.sh`);
-    },
-    default: () => {
-      program.help();
-    }
-  };
-  return (options[option] || options['default'])();
-}
