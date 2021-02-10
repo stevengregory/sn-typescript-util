c8848e47c5a4fc9b3fdbb269931893d480ad0b47
Steven Gregory <sgregory@gmail.com>
2021-02-09 19:57:56 -0600

    refactor: extract logic to functions

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,40 @@ const path = require('path');
 const { description, version } = require('./../package.json');
 const { program } = require('commander');
 
-program.description(description);
-program.version(version);
-program.option(
-  '-b, --build',
-  'Build project utility files & package dependencies'
-);
-program.option(
-  '-c, --compile',
-  'Compile TypeScript files to JavaScript & move to src'
-);
-program.option(
-  '-s, --sync',
-  'Sync new instance-based src files to the ts directory'
-);
-program.parse(process.argv).opts();
+(() => {
+  program.description(description);
+  program.version(version);
+  program.option(
+    '-b, --build',
+    'Build project utility files & package dependencies'
+  );
+  program.option(
+    '-c, --compile',
+    'Compile TypeScript files to JavaScript & move to src'
+  );
+  program.option(
+    '-s, --sync',
+    'Sync new instance-based src files to the ts directory'
+  );
+  program.parse(process.argv).opts();
+  getOption(program.opts());
+})();
 
-const options = program.opts();
-if (options.build) {
-  childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/build.sh`);
+function getOption(opts) {
+  const option = Object.keys(opts).toString();
+  const options = {
+    build: function () {
+      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/build.sh`);
+    },
+    compile: function () {
+      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/compile.sh`);
+    },
+    sync: function () {
+      childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/sync.sh`);
+    },
+    default: function () {
+      program.help();
+    }
+  };
+  return (options[option] || options['default'])();
 }
-if (options.compile) {
-  childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/compile.sh`);
-}
-if (options.sync) {
-  childProcess.exec(`sh ${path.join(__dirname, '../scripts')}/sync.sh`);
-}
-
-program.help();
