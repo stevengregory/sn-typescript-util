f9be631ccdff522988e22d0a07e4c831ad8df4d0
Steven Gregory <sgregory@gmail.com>
2021-02-12 23:23:12 -0600

    refactor: update method names & order

--- scripts/build.rb
+++ scripts/build.rb
@@ -4,6 +4,16 @@ require_relative File.join(__dir__, 'utils')
 
 module ServiceNow
   class Build
+    def add_packages
+      %x( npm i @types/servicenow @types/node commander npm-add-script nodemon prettier typescript ts-node -g )
+      %x( npm i @types/servicenow @types/node commander nodemon prettier ts-node typescript -D )
+    end
+
+    def add_package_scripts
+      %x( npmAddScript -k start -v "npm run snt:watch" )
+      %x( npmAddScript -k snt:watch -v "nodemon --watch 'ts/**/*.ts' --exec 'snt -c' -e ts" )
+    end
+
     def create_prettier_config
       config = '.prettierrc.json'
       file = ServiceNow::Utils.new.fetch_file 'scripts', config
@@ -17,19 +27,9 @@ module ServiceNow
       ServiceNow::Utils.new.replace_content config
     end
 
-    def install_packages
-      %x( npm i @types/servicenow @types/node commander npm-add-script nodemon prettier typescript ts-node -g )
-      %x( npm i @types/servicenow @types/node commander nodemon prettier ts-node typescript -D )
-    end
-
-    def update_package_scripts
-      %x( npmAddScript -k start -v "npm run snt:watch" )
-      %x( npmAddScript -k snt:watch -v "nodemon --watch 'ts/**/*.ts' --exec 'snt -c' -e ts" )
-    end
-
     def start
-      install_packages
-      update_package_scripts
+      add_packages
+      add_package_scripts
       create_prettier_config
       create_tsconfig
     end
