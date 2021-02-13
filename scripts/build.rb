9fb93b2df50124aebf3d462912da15040fea5070
Steven Gregory <sgregory@gmail.com>
2021-02-12 23:19:49 -0600

    refactor: update build method names

--- scripts/build.rb
+++ scripts/build.rb
@@ -4,22 +4,22 @@ require_relative File.join(__dir__, 'utils')
 
 module ServiceNow
   class Build
-    def install_packages
-      %x( npm i @types/servicenow @types/node commander npm-add-script nodemon prettier typescript ts-node -g )
-      %x( npm i @types/servicenow @types/node commander nodemon prettier ts-node typescript -D )
-    end
-
-    def make_prettier_config
+    def create_prettier_config
       config = '.prettierrc.json'
       file = ServiceNow::Utils.new.fetch_file 'scripts', config
       File.write(config, file)
     end
 
-    def make_tsconfig_file
+    def create_tsconfig
       config = 'tsconfig.json'
       file = ServiceNow::Utils.new.fetch_file 'scripts', config
       File.write(config, file)
       ServiceNow::Utils.new.replace_content config
+    end
+
+    def install_packages
+      %x( npm i @types/servicenow @types/node commander npm-add-script nodemon prettier typescript ts-node -g )
+      %x( npm i @types/servicenow @types/node commander nodemon prettier ts-node typescript -D )
     end
 
     def update_package_scripts
@@ -30,8 +30,8 @@ module ServiceNow
     def create
       install_packages
       update_package_scripts
-      make_prettier_config
-      make_tsconfig_file
+      create_prettier_config
+      create_tsconfig
     end
   end
 end
