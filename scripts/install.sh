9f7c9e2dc536f0e3cfd682af2f2e52b78df9b3d9
Steven Gregory <sgregory@gmail.com>
2021-03-02 20:21:47 -0600

    build: optimize install & update nodemon

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 
 readonly packages=(
-  '@types/servicenow'
-  '@types/node'
-  'colorette'
-  'commander'
-  'npm-add-script'
   'nodemon'
+  'npm-add-script'
   'prettier'
   'typescript'
-  'ts-node'
 )
 
 for p in "${packages[@]}"
 do
-  npm i $p -D
+  npm i -g $p
 done
 
-npm i -g nodemon
-npm i -g npm-add-script
 npmAddScript -k watch -v "nodemon --exec snts -c"
