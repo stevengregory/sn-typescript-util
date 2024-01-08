741b25e47081ea74205f234a508c371e9604c888
stevengregory <sgregory@gmail.com>
2024-01-08 15:27:53 -0600

    chore: update sync exclude directory

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -15,14 +15,14 @@ get_project_name() {
 }
 
 sync() {
-  interface_dir="Interfaces"
+  types_dir="Types"
   src_path="$(get_project_name system/sn-workspace.json ACTIVE_APPLICATION)/src"
   ts_path="$(get_project_name system/sn-workspace.json ACTIVE_APPLICATION)/ts"
   clean_build "$build_dir"
   if [ -d "$ts_path" ]; then
     find "$ts_path" -name "*.ts" -exec sh -c 'mv "$0" "${0%.ts}.js"' {} \;
   fi
-  rsync --ignore-existing --delete-after -raz --progress --prune-empty-dirs --include "*/" --include "*.js" --exclude="*" "$interface_dir" "$src_path/" "$ts_path"
+  rsync --ignore-existing --delete-after -raz --progress --prune-empty-dirs --include "*/" --include "*.js" --exclude="*" "$types_dir" "$src_path/" "$ts_path"
   find "$ts_path" -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.ts"' {} \;
 }
 
