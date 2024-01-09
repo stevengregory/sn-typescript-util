b3b641a31739a5d2e8a0bd2f32f661af34260bba
stevengregory <sgregory@gmail.com>
2024-01-09 17:19:03 -0600

    feat: add metadata interface & update workspace

---
+++ scripts/types/metadata.ts
@@ -0,0 +1,8 @@
+export interface Metadata {
+  readonly sys_id: string;
+  readonly sys_scope: string;
+  readonly package_type: string;
+  readonly PROJECT_STATE: string;
+  readonly INSTANCE_ID: string;
+  readonly BUILD_NAME: string;
+}
--- scripts/types/workspace.ts
+++ scripts/types/workspace.ts
@@ -1,4 +1,8 @@
+import { Metadata } from './metadata.js';
+
 export interface Workspace {
+  readonly ALL_APPLICATIONS: {
+    [appName: string]: Metadata;
+  };
   readonly ACTIVE_APPLICATION: string;
-  readonly ALL_APPLICATIONS: string;
 }
