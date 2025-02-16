d04bf49ebcfdf6295d033abc6957fa5161a9429f
Steven Gregory <sgregory@gmail.com>
2025-02-16 11:58:22 -0600

    refactor(base-table): improve column structure & typing

--- scripts/templates/base-table.ts
+++ scripts/templates/base-table.ts
@@ -1,8 +1,24 @@
+interface Column<Type = string, Default extends Type = Type> {
+  default?: Default | string;
+  maxLength?: number;
+  label?: string;
+  mandatory?: boolean;
+  read_only?: boolean;
+  active?: boolean;
+  audit?: boolean;
+  attributes?: Record<string, string | number | boolean>;
+  function_definition?: `glidefunction:${string}`;
+}
+
+type StringColumn = Column<string>;
+type IntegerColumn = Column<number>;
+type GenericColumn<T> = Column<T> & { column_type: string };
+
 interface BaseTable {
-  readonly sys_created_on?: Date;
-  readonly sys_created_by?: string;
-  readonly sys_id?: string;
-  readonly sys_mod_count?: number;
-  readonly sys_updated_by?: string;
-  readonly sys_updated_on?: Date;
+  sys_id?: GenericColumn<string>;
+  sys_created_on?: GenericColumn<string>;
+  sys_created_by?: StringColumn;
+  sys_mod_count?: IntegerColumn;
+  sys_updated_by?: StringColumn;
+  sys_updated_on?: GenericColumn<string>;
 }
